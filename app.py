d8f32b5123173074339fd47e11e553bf851e9e59
EdgarVRP <edgarvrp22@gmail.com>
2023-05-06 14:40:59 -0600

    Se configura app.py importanto os

--- app.py
+++ app.py
@@ -9,20 +9,9 @@ from flask import Flask
 app = Flask(__name__)
 CORS(app)
 
-if __name__ == '__main__':
-    PORT = int(os.environ.get("PORT", 5000))
-    app.run(host='localhost', port=PORT)
-    #se imprime en consola el puerto en el que se esta ejecutando la aplicacion
-
-#Se muestra el mensaje de bienvenida
-@app.route('/')
-def hello_world():
-    return 'Bienvenido a la API de Evaluaciones de Prestatarios'
-#Se muestra el puerto en el que se esta ejecutando la aplicacion
-@app.route('/port')
-def port():
-    return 'El puerto es: ' + str(PORT)
-
 api = Api(app)
 api.add_resource(EvaluacionController, "/Evaluaciones", "/Evaluaciones/<string:idPrestatario>")
 
+if __name__ == '__main__':
+    PORT = int(os.environ.get('PORT', 5000))
+    app.run(host='localhost', port=PORT)
